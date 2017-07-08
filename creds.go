125f1fa4fdded40878ec49302ea4e8433b3a6de8
Graham Davison <gdavison@paybyphone.com>
2017-07-07 23:37:46 -0700

    Wrapped default password generation

--- creds.go
+++ creds.go
@@ -1,12 +1,12 @@
 package oracle
 
 import (
+	"math/rand"
+	"strings"
 	"time"
 
-	regen "github.com/gdavison/crypto-goregen"
 	"github.com/hashicorp/vault/builtin/logical/database/dbplugin"
 	"github.com/hashicorp/vault/plugins/helper/database/credsutil"
-	"strings"
 )
 
 // oracleCredentialsProducer implements CredentialsProducer.
@@ -22,22 +22,28 @@ func (ocp *oracleCredentialsProducer) GenerateUsername(config dbplugin.UsernameC
 	}
 }
 
-// Oracle passwords: https://asktom.oracle.com/pls/apex/f?p=100:11:0::::P11_QUESTION_ID:595223460734
-// o Passwords must be from 1 to 30 characters long.
-// o Passwords cannot contain quotation marks.
-// o Passwords are not case sensitive.
-// o A Password must begin with an alphabetic character.
-// o Passwords can contain only alphanumeric characters and the
-// underscore (_), dollar sign ($), and pound sign (#). Oracle
-// strongly discourages you from using $ and #..
-// o A Password cannot be an Oracle reserved word (eg: SELECT).
 func (ocp *oracleCredentialsProducer) GeneratePassword() (string, error) {
-	password, err := regen.Generate("[[:lower:]][_[:lower:][:digit:]]{29}")
-	if err != nil {
+	// Oracle passwords: https://asktom.oracle.com/pls/apex/f?p=100:11:0::::P11_QUESTION_ID:595223460734
+	// o Passwords must be from 1 to 30 characters long.
+	// o Passwords cannot contain quotation marks.
+	// o Passwords are not case sensitive.
+	// o A Password must begin with an alphabetic character.
+	// o Passwords can contain only alphanumeric characters and the
+	//   underscore (_), dollar sign ($), and pound sign (#). Oracle
+	//   strongly discourages you from using $ and #.
+	// o A Password cannot be an Oracle reserved word (eg: SELECT).
+	if password, err := ocp.SQLCredentialsProducer.GeneratePassword(); err != nil {
 		return "", err
+	} else {
+		// credsutil.SQLCredentialsProducer.GeneratePassword() uses github.com/hashicorp/go-uuid, which generates
+		// cryptographically-random UUIDs. We should be safe replacing the first character with a non-secure
+		// random lower-case character
+		charOffset := rand.Intn(26)
+		char := string(rune('a') + rune(charOffset))
+		password = strings.Replace(password, "-", "", -1)
+		password = char + password[1:oraclePasswordLength]
+		return password, nil
 	}
-
-	return password, nil
 }
 
 func (ocp *oracleCredentialsProducer) GenerateExpiration(ttl time.Time) (string, error) {
--- creds_test.go
+++ creds_test.go
@@ -1,16 +1,20 @@
 package oracle
 
 import (
+	"fmt"
+	"math/rand"
+	"reflect"
+	"regexp"
 	"testing"
 	"testing/quick"
 
-	"fmt"
 	"github.com/hashicorp/vault/builtin/logical/database/dbplugin"
 	"github.com/hashicorp/vault/plugins/helper/database/credsutil"
-	"regexp"
 )
 
-func TestUsernameShouldMatchOracleRequirements(t *testing.T) {
+type testUsername string
+
+func (tu testUsername) Generate(rand *rand.Rand, size int) reflect.Value {
 	credsProducer := &oracleCredentialsProducer{
 		credsutil.SQLCredentialsProducer{
 			DisplayNameLen: oracleDisplayNameMaxLength,
@@ -23,14 +27,41 @@ func TestUsernameShouldMatchOracleRequirements(t *testing.T) {
 		DisplayName: "display",
 		RoleName:    "role",
 	}
+	if username, err := credsProducer.GenerateUsername(usernameConfig); err != nil {
+		panic(err)
+	} else {
+		return reflect.ValueOf(testUsername(username))
+	}
+}
 
-	assertion := func() bool {
-		username, _ := credsProducer.GenerateUsername(usernameConfig)
-		result, _ := regexp.MatchString(fmt.Sprintf("[[:lower:]][_[:lower:][:digit:]]{%d}", oracleUsernameLength-1), username)
+func TestUsernameShouldMatchOracleRequirements(t *testing.T) {
+	assertion := func(username testUsername) bool {
+		result, _ := regexp.MatchString(fmt.Sprintf("^[[:lower:]][_[:lower:][:digit:]]{%d}$", oracleUsernameLength-1), string(username))
 		return result
 	}
 	if err := quick.Check(assertion, nil); err != nil {
 		t.Error(err)
 	}
+}
 
+type testPassword string
+
+func (tp testPassword) Generate(rand *rand.Rand, size int) reflect.Value {
+	credsProducer := &oracleCredentialsProducer{}
+	if password, err := credsProducer.GeneratePassword(); err != nil {
+		panic(err)
+	} else {
+		return reflect.ValueOf(testPassword(password))
+	}
+}
+
+func TestPasswordShouldMatchOracleRequirements(t *testing.T) {
+
+	assertion := func(password testPassword) bool {
+		result, _ := regexp.MatchString(fmt.Sprintf("^[[:lower:]][_[:lower:][:digit:]]{%d}$", oraclePasswordLength-1), string(password))
+		return result
+	}
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error(err)
+	}
 }
