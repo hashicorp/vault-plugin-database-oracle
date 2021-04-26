fa8ef820472cb006463c8d483aa058edfe21b9d8
Michael Golowka <72365+pcman312@users.noreply.github.com>
2021-04-26 11:01:17 -0600

    Update go-oci8 to v0.1.1

--- vendor/github.com/mattn/go-oci8/connector.go
+++ vendor/github.com/mattn/go-oci8/connector.go
@@ -11,24 +11,28 @@ import (
 
 // NewConnector returns a new database connector
 func NewConnector(hosts ...string) driver.Connector {
-	return &OCI8Connector{
+	return &Connector{
 		Logger: log.New(ioutil.Discard, "", 0),
 	}
 }
 
 // Driver returns the OCI8 driver
-func (oci8Connector *OCI8Connector) Driver() driver.Driver {
-	return OCI8Driver
+func (connector *Connector) Driver() driver.Driver {
+	return Driver
 }
 
 // Connect returns a new database connection
-func (oci8Connector *OCI8Connector) Connect(ctx context.Context) (driver.Conn, error) {
-	oci8Conn := &OCI8Conn{
-		logger: oci8Connector.Logger,
-	}
-	if oci8Conn.logger == nil {
-		oci8Conn.logger = log.New(ioutil.Discard, "", 0)
+func (connector *Connector) Connect(ctx context.Context) (driver.Conn, error) {
+	if ctx.Err() != nil {
+		return nil, ctx.Err()
 	}
 
-	return oci8Conn, nil
+	conn := &Conn{
+		logger: connector.Logger,
+	}
+	if conn.logger == nil {
+		conn.logger = log.New(ioutil.Discard, "", 0)
+	}
+
+	return conn, nil
 }
