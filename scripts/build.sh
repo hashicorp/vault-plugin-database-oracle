841ad0ae83024a5fe7908f079f65c6d667861c04
Christopher Swenson <swenson@swenson.io>
2022-03-22 09:21:13 -0700

    VAULT-5038 Convert to CRT for releases (#75)
    VAULT-5038 Convert to CRT for releases

--- scripts/build.sh
+++ scripts/build.sh
@@ -64,20 +64,7 @@ if [ "${VAULT_DEV_BUILD}x" != "x" ]; then
     done
 fi
 
-if [ "${VAULT_DEV_BUILD}x" = "x" ]; then
-    # Zip and copy to the dist dir
-    echo "==> Packaging..."
-    for PLATFORM in $(find ./pkg/bin -mindepth 1 -maxdepth 1 -type d); do
-        OSARCH=$(basename ${PLATFORM})
-        echo "--> ${OSARCH}"
-
-        pushd $PLATFORM >/dev/null 2>&1
-        zip ../${OSARCH}.zip ./*
-        popd >/dev/null 2>&1
-    done
-fi
-
 # Done!
 echo
 echo "==> Results:"
-ls -hl pkg/
+ls -ahlR pkg/
